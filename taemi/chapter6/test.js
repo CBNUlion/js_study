8893ae21bc444f0aeb170acd4f6030f11b032ba0
taemi <dmswl1002@naver.com>
2019-07-03 14:15:15 +0900

    chapter6 어렵다

---
+++ taemi/chapter6/test.js
@@ -0,0 +1,22 @@
+//객체
+/* function f(o){
+    o.message = "f에서 수정함";
+    o = {
+        message : "새로운 객체!"
+    };
+    console.log(`f 내부 : o.message = "${o.message}"(할당후)`);
+}
+
+let o = {
+    message:'초기값'
+};
+
+console.log(`f를 호출하기 전 : o.message = "${o.message}"`);
+f(o);
+console.log(`f를 호출한 다음 : o.message="${o.message}"`);
+
+// 처음 매개변수로 받았을 때는 둘이 같은 객체를 가리키지만 내부적으로 새로운 객체를 넣어줬을 때에는 값은 같지만 다른 객체로서 함수 밖의  o는 여전히 원래의 o를 가리키고 있다.
+*/
+
+
+
