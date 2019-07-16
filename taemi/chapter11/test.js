1c5f428fa58330b7297169aedcf7893665b8dc64
taemi <dmswl1002@naver.com>
2019-07-16 19:27:54 +0900

    chapter 12,13

--- taemi/chapter11/test.js
+++ taemi/chapter11/test.js
@@ -1,8 +1,22 @@
-const x = 1;
-console.log(x);
-function f(){
-    console.log(x);
-    const x = 1;
+// const x = 1;
+// console.log(x);
+// function f(){
+//     console.log(x);
+//     const x = 1;
+// }
+
+// f();
+
+
+
+//catch 에 매개변수를 안 넣어줘도 에러를 잡아준다.
+try{
+    asdsas
+    console.log("asd");
+    aa
+} catch {
+    console.log("err!");
 }
 
-f();
+
+
